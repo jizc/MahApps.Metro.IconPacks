31ec03feb2194ccd8e7bbe02ce0eff6a31695beb
Jan Ivar Z. Carlsen <jizc@outlook.com>
2022-08-08 19:19:46 +0200

    More project changes

--- src/MahApps.Metro.IconPacks.Core/PathIconBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using Windows.UI.Xaml.Controls;
-
-namespace MahApps.Metro.IconPacks
-{
-    public abstract class PathIconBase : PathIcon
-    {
-        protected abstract void UpdateData();
-    }
-}
