14c8c3d56d94ca4d38cfdaf9f274e884585c6613
Jan Ivar Z. Carlsen <jizc@outlook.com>
2023-09-27 15:10:32 +0200

    Use ints for keys in dictionary

--- src/IconGenerator/Program.cs
+++ src/IconGenerator/Program.cs
@@ -1,4 +1,79 @@
-﻿string ToCamelCase(string source)
+﻿const string kindTemplate =
+    """
+    using System.ComponentModel;
+
+    namespace MahApps.Metro.IconPacks;
+
+    /// ******************************************
+    /// This code is auto generated. Do not amend.
+    /// ******************************************
+
+    /// <summary>
+    /// List of available icons for use with <see cref="PackIconMaterial" />.
+    /// </summary>
+    /// <remarks>
+    /// All icons sourced from Material Design Icons Font <see><cref>https://materialdesignicons.com</cref></see>
+    /// In accordance of <see><cref>https://github.com/Templarian/MaterialDesign/blob/master/LICENSE</cref></see>.
+    /// </remarks>
+    public enum PackIconMaterialKind
+    {
+    INSERT_HERE}
+
+    """;
+
+const string solutionDir = @"..\..\..\..\..\";
+const string svgDir = solutionDir + @"..\..\MaterialDesignSvgo\svg";
+
+var icons = new List<(string Name, string Path)> { ("None", string.Empty) };
+
+Console.WriteLine("Reading icon data...");
+
+foreach (var path in Directory.EnumerateFiles(svgDir))
+{
+    var fileName = Path.GetFileNameWithoutExtension(path);
+    var iconName = ToCamelCase(fileName);
+    if (iconName is "CheckBoxMultipleOutline" or "CheckBoxOutline")
+    {
+        continue;
+    }
+
+    var text = File.ReadAllText(path);
+    var svgIndex = text.IndexOf("path d=\"", StringComparison.Ordinal);
+    var svg = text[(svgIndex + 8)..^9];
+    icons.Add((ToCamelCase(fileName), svg));
+
+    if (icons.Count % 500 is 0)
+    {
+        Console.WriteLine($"Reading icon data {icons.Count}...");
+    }
+}
+
+Console.WriteLine($"Total icons: {icons.Count}");
+Console.WriteLine("\nGenerating icon kind...");
+
+var stringBuilder = new System.Text.StringBuilder();
+
+foreach (var (name, _) in icons)
+{
+    stringBuilder.AppendLine($"    {name},");
+}
+
+var contents = kindTemplate.Replace("INSERT_HERE", stringBuilder.ToString());
+stringBuilder.Clear();
+File.WriteAllText(solutionDir + @"src\MahApps.Metro.IconPacks\PackIconMaterialKind.cs", contents);
+
+Console.WriteLine("Generating json...");
+var dictionary = new Dictionary<int, string>();
+for (var i = 0; i < icons.Count; ++i)
+{
+    dictionary.Add(i, icons[i].Path);
+}
+
+var json = System.Text.Json.JsonSerializer.Serialize(dictionary);
+File.WriteAllText(solutionDir + @"src\MahApps.Metro.IconPacks\Resources\PackIconMaterialData.json", json);
+return;
+
+static string ToCamelCase(string source)
 {
     source = char.ToUpperInvariant(source[0]) + source[1..];
     while (source.Contains('-'))
@@ -27,65 +102,3 @@
 
     return source;
 }
-
-const string kindTemplate = @"using System.ComponentModel;
-
-namespace MahApps.Metro.IconPacks;
-
-/// ******************************************
-/// This code is auto generated. Do not amend.
-/// ******************************************
-
-/// <summary>
-/// List of available icons for use with <see cref=""PackIconMaterial"" />.
-/// </summary>
-/// <remarks>
-/// All icons sourced from Material Design Icons Font <see><cref>https://materialdesignicons.com</cref></see>
-/// In accordance of <see><cref>https://github.com/Templarian/MaterialDesign/blob/master/LICENSE</cref></see>.
-/// </remarks>
-public enum PackIconMaterialKind
-{
-INSERT_HERE}
-";
-
-const string solutionDir = @"..\..\..\..\..\";
-const string svgDir = solutionDir + @"..\..\MaterialDesignSvgo\svg";
-
-var icons = new Dictionary<string, string> { { "None", string.Empty } };
-
-Console.WriteLine("Reading icon data...");
-
-foreach (var path in Directory.EnumerateFiles(svgDir))
-{
-    var fileName = Path.GetFileNameWithoutExtension(path);
-    var text = File.ReadAllText(path);
-    var svgIndex = text.IndexOf("path d=\"", StringComparison.Ordinal);
-    var svg = text[(svgIndex + 8)..^9];
-    icons[ToCamelCase(fileName)] = svg;
-
-    if (icons.Count % 500 is 0)
-    {
-        Console.WriteLine($"Reading icon data {icons.Count}...");
-    }
-}
-
-icons.Remove("CheckBoxMultipleOutline");
-icons.Remove("CheckBoxOutline");
-
-Console.WriteLine($"Total icons: {icons.Count}");
-Console.WriteLine("\nGenerating icon kind...");
-
-var stringBuilder = new System.Text.StringBuilder();
-
-foreach (var icon in icons.Keys)
-{
-    stringBuilder.AppendLine($"    {icon},");
-}
-
-var contents = kindTemplate.Replace("INSERT_HERE", stringBuilder.ToString());
-stringBuilder.Clear();
-File.WriteAllText(solutionDir + @"src\MahApps.Metro.IconPacks\PackIconMaterialKind.cs", contents);
-
-Console.WriteLine("Generating json...");
-var json = System.Text.Json.JsonSerializer.Serialize(icons);
-File.WriteAllText(solutionDir + @"src\MahApps.Metro.IconPacks\Resources\PackIconMaterialData.json", json);
